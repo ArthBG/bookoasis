363b5711ef2ba576b059a42acde3163091707438
ArthBG <arthurborges.senai564@gmail.com>
2024-11-17 15:46:33 -0300

    feat: Add Book type definition for type safety in book management

---
+++ src/types/Book.ts
@@ -0,0 +1,14 @@
+export type Book = {
+    id: number;
+    title: string;
+    category: string;
+    synopsis: string;
+    numberOfPages: number;
+    author: string;
+    releaseDate: Date;
+    publisher: string;
+    isbn: string;
+    language: string;
+    edition: string;
+    coverURL: string;
+};
