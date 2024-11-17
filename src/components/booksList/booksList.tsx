984a172a1d725bdc38e19e9a6372657809ae7892
ArthBG <arthurborges.senai564@gmail.com>
2024-11-17 15:46:30 -0300

    feat: Implement BooksList component to fetch and display books

---
+++ src/components/booksList/booksList.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { Book } from '@/types/Book'
+
+const BooksList = () => {
+    const [books, setBooks] = useState<Book[]>([])
+
+    useEffect(() => {
+        const getBooks = async () => {
+            const response = await fetch('/api/v1/books')
+            console.log(response)
+            const data = await response.json()
+            setBooks(data)
+        }
+        getBooks()
+    }, [])
+
+    return (
+        <div>
+            {books.map((book) => (
+                <div key={book.id}>
+                    <h2>{book.title}</h2>
+                    <p>{book.synopsis}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default BooksList;
