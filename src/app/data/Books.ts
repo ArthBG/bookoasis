a7f6aa2a0b3470967aea0754674939f901e4957d
ArthBG <arthurborges.senai564@gmail.com>
2024-11-17 15:46:26 -0300

    feat: Add initial book data to the application

---
+++ src/app/data/Books.ts
@@ -0,0 +1,257 @@
+export const books = [
+    {
+      bookId: 1,
+      title: '1984',
+      category: 'Ficção Distópica',
+      synopsis:
+        "Winston, herói de 1984 , último romance de George Orwell, vive aprisionado na engrenagem totalitária de uma sociedade completamente dominada pelo Estado, onde tudo é feito coletivamente, mas cada qual vive sozinho. Ninguém escapa à vigilância do Grande Irmão, a mais famosa personificação literária de um poder cínico e cruel ao infinito, além de vazio de sentido histórico. De fato, a ideologia do Partido dominante em Oceânia não visa nada de coisa alguma para ninguém, no presente ou no futuro. O'Brien, hierarca do Partido, é quem explica a Winston que só nos interessa o poder em si. Nem riqueza, nem luxo, nem vida longa, nem felicidade: só o poder pelo poder, poder puro",
+      numberOfPages: 328,
+      author: 'George Orwell',
+      launch: new Date('1949-06-08'),
+      publisher: 'Secker & Warburg',
+      language: 'Português',
+      edition: 1,
+      cover: 'https://i.imgur.com/p6cDFpj.jpg',
+      isbn: '9780451524935'
+    },
+    {
+      bookId: 2,
+      title: 'Dom Casmurro',
+      category: 'Romance',
+      synopsis:
+        'A famosa história do amor entre Capitu e Bentinho já faz parte do nosso imaginário. Mas tão importante quanto o enredo genial de Dom Casmurro é a sua narrativa singular, que reafirmou o estilo magistral de Machado de Assis. Caro leitor amigo, com esta edição anotada e acompanhada de roteiro de leitura, renovamos o convite do autor, parodiando o final deste romance: “Vamos ao Dom Casmurro.',
+      numberOfPages: 286,
+      author: 'Machado de Assis',
+      launch: new Date('1899-01-01'),
+      publisher: 'Typ. de Paula Brito',
+      language: 'Português',
+      edition: 1,
+      cover: 'https://i.imgur.com/GciBc3Y.jpg',
+      isbn: '9788572328759'
+    },
+    {
+      bookId: 3,
+      title: 'O Senhor dos Anéis: A Sociedade do Anel',
+      category: 'Fantasia',
+      synopsis:
+        'A Sociedade do Anel começa no Condado, a região rural do oeste da Terra-média onde vivem os diminutos e pacatos hobbits. Bilbo Bolseiro, um dos raros aventureiros desse povo, cujas peripécias foram contadas em O Hobbit, resolve ir embora do Condado e deixa sua considerável herança nas mãos de seu jovem parente Frodo.',
+      numberOfPages: 707,
+      author: 'J.R.R. Tolkien',
+      launch: new Date('1954-07-29'),
+      publisher: 'Allen & Unwin',
+      language: 'Português',
+      edition: 1,
+      cover: 'https://i.imgur.com/UtvnKTd.jpg',
+      isbn: '9780547928210'
+    },
+    {
+      bookId: 4,
+      title: 'Orgulho e Preconceito',
+      category: 'Romance',
+      synopsis:
+        'A chegada de novos vizinhos provoca um alvoroço na vizinhança ao apresentar dois jovens solteiros e abastados: o senhor Bingley, por quem Jane Bennet, a primogênita, logo se apaixona; e o senhor Darcy, um homem orgulhoso que desperta o desprezo de Elizabeth Bennet, a segunda irmã mais velha e heroína desta história.',
+      numberOfPages: 279,
+      author: 'Jane Austen',
+      launch: new Date('1813-01-28'),
+      publisher: 'T. Egerton, Whitehall',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/ihtRYRR.jpg',
+      isbn: '9780141439518'
+    },
+    {
+      bookId: 5,
+      title: 'O Apanhador no Campo de Centeio',
+      category: 'Ficção',
+      synopsis:
+        'Com uns trocados da venda de uma máquina de escrever e portando seu indefectível boné vermelho de caçador, o jovem traça um plano incerto: tomar um trem para Nova York e vagar por três dias pela grande cidade, adiando a volta à casa dos pais até que eles recebam a notícia da expulsão por alguém da escola.',
+      numberOfPages: 277,
+      author: 'J.D. Salinger',
+      launch: new Date('1951-07-16'),
+      publisher: 'Little, Brown and Company',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/chNTKnt.jpg',
+      isbn: '9780316769488'
+    },
+    {
+      bookId: 6,
+      title: 'Moby Dick',
+      category: 'Aventura',
+      synopsis:
+        'O livro traz o relato de um marinheiro letrado, Ishmael, sobre a última viagem de um navio baleeiro de Nantucket, o Pequod, que parte da costa leste dos Estados Unidos - com sua tripulação multiétnica - rumo ao Pacífico Sul, onde encontra o imenso cachalote branco que, no passado, arrancara a perna do vingativo capitão',
+      numberOfPages: 585,
+      author: 'Herman Melville',
+      launch: new Date('1851-10-18'),
+      publisher: 'Harper & Brothers',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/p8zjXIY.jpg',
+      isbn: '9781503280786'
+    },
+    {
+      bookId: 7,
+      title: 'Cem Anos de Solidão',
+      category: 'Realismo Mágico',
+      synopsis:
+        'Em Cem anos de solidão , um dos maiores clássicos da literatura, o prestigiado autor narra a incrível e triste história dos Buendía - a estirpe de solitários para a qual não será dada “uma segunda oportunidade sobre a terra” e apresenta o maravilhoso universo da fictícia Macondo, onde se passa o romance.',
+      numberOfPages: 417,
+      author: 'Gabriel García Márquez',
+      launch: new Date('1967-05-30'),
+      publisher: 'Harper & Row',
+      language: 'Espanhol',
+      edition: 1,
+      cover: 'https://i.imgur.com/KbfV5We.jpg',
+      isbn: '9780060883287'
+    },
+    {
+      bookId: 8,
+      title: 'O Grande Gatsby',
+      category: 'Romance',
+      synopsis:
+        'O grande Gatsby é uma brilhante recriação da alta sociedade norte-americana na Era do Jazz. Remove camada por camada desse mundo glamoroso e revela a insensibilidade e crueldade do seu âmago. Como diz Ruy Castro na apresentação que preparou especialmente para esta edição, Gatsby é o alter ego do próprio F.',
+      numberOfPages: 180,
+      author: 'F. Scott Fitzgerald',
+      launch: new Date('1925-04-10'),
+      publisher: "Charles Scribner's Sons",
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/SY8zjb4.jpg',
+      isbn: '9780743273565'
+    },
+    {
+      bookId: 9,
+      title: 'Crime e Castigo',
+      category: 'Ficção',
+      synopsis:
+        'Trata-se de um enredo de suspense e de grande tensão, de uma profundidade psicológica única, passado na turbulenta Rússia tsarista do século XIX. Raskólhnikov é um jovem pobre, ex-estudante da universidade, que vive nos bairros marginais de São Petersburgo.',
+      numberOfPages: 671,
+      author: 'Fiódor Dostoiévski',
+      launch: new Date('1866-01-01'),
+      publisher: 'The Russian Messenger',
+      language: 'Russo',
+      edition: 1,
+      cover: 'https://i.imgur.com/Lc7zRTl.jpg',
+      isbn: '9780140449136'
+    },
+    {
+      bookId: 10,
+      title: 'O Nome do Vento',
+      category: 'Fantasia',
+      synopsis:
+        'Um jovem garoto membro de trupe de artistas que busca vingança pela morte de sua família pelas mãos de um grupo folclórico chamado "Chandriano". Até ai, o cerne da narrativa é apenas mais um clichê das histórias.',
+      numberOfPages: 662,
+      author: 'Patrick Rothfuss',
+      launch: new Date('2007-03-27'),
+      publisher: 'DAW Books',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/UOVVUHX.jpg',
+      isbn: '9780756404741'
+    },
+    {
+      bookId: 11,
+      title: 'Harry Potter e a Pedra Filosofal',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter é um garoto órfão que vive infeliz com seus tios, os Dursleys. Ele recebe uma carta contendo um convite para ingressar em Hogwarts, uma famosa escola especializada em formar jovens bruxos. Inicialmente, Harry é impedido de ler a carta por seu tio, mas logo recebe a visita de Hagrid, o guarda-caça de Hogwarts, que chega para levá-lo até a escola. Harry adentra um mundo mágico que jamais imaginara, vivendo diversas aventuras com seus novos amigos, Rony Weasley e Hermione Granger.',
+      numberOfPages: 264,
+      author: 'J.K. Rowling',
+      launch: new Date('1997-06-26'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/ItoYRbI.jpg',
+      isbn: '9780747532699'
+    },
+    {
+      bookId: 12,
+      title: 'Harry Potter e a Câmara Secreta',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter está de volta para o segundo ano em Hogwarts, onde ele ouve rumores de que a Câmara Secreta foi aberta e que o herdeiro de Sonserina irá matar todos os nascidos trouxas. Harry e seus amigos Rony e Hermione decidem investigar o mistério, mas o garoto acaba sendo o principal suspeito de ser o herdeiro de Sonserina.',
+      numberOfPages: 224,
+      author: 'J.K. Rowling',
+      launch: new Date('1998-07-02'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/30A8EgQ.jpg',
+      isbn: '9780747538493'
+    },
+    {
+      bookId: 13,
+      title: 'Harry Potter e o Prisioneiro de Azkaban',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter está de volta para o terceiro ano em Hogwarts, onde ele descobre que Sirius Black, um assassino em série, está atrás dele. Harry e seus amigos Rony e Hermione decidem investigar o mistério, mas o garoto acaba descobrindo que Sirius Black é seu padrinho e que ele foi injustamente acusado de traição.',
+      numberOfPages: 348,
+      author: 'J.K. Rowling',
+      launch: new Date('1999-07-08'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/WJ61l9V.jpg',
+      isbn: '9780747542155'
+    },
+    {
+      bookId: 14,
+      title: 'Harry Potter e o Cálice de Fogo',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter está de volta para o quarto ano em Hogwarts, onde ele é selecionado para participar do Torneio Tribruxo. Harry e seus amigos Rony e Hermione decidem investigar o mistério, mas o garoto acaba descobrindo que o torneio foi armado para trazer de volta Lord Voldemort.',
+      numberOfPages: 536,
+      author: 'J.K. Rowling',
+      launch: new Date('2000-07-08'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/8IfA5Mn.jpg',
+      isbn: '9780747546245'
+    },
+    {
+      bookId: 15,
+      title: 'Harry Potter e a Ordem da Fênix',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter está de volta para o quinto ano em Hogwarts, onde ele descobre que o Ministério da Magia está tentando esconder a verdade sobre Lord Voldemort. Harry e seus amigos Rony e Hermione decidem investigar o mistério, mas o garoto acaba descobrindo que o Ministério da Magia está tentando controlar a escola.',
+      numberOfPages: 703,
+      author: 'J.K. Rowling',
+      launch: new Date('2003-06-21'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/8blJ5RW.jpg',
+      isbn: '9780747551003'
+    },
+    {
+      bookId: 16,
+      title: 'Harry Potter e o Enigma do Príncipe',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter está de volta para o sexto ano em Hogwarts, onde ele descobre que Lord Voldemort está atrás de um segredo que pode destruir o mundo mágico. Harry e seus amigos Rony e Hermione decidem investigar o mistério, mas o garoto acaba descobrindo que o segredo está ligado a um antigo aluno da escola.',
+      numberOfPages: 432,
+      author: 'J.K. Rowling',
+      launch: new Date('2005-07-16'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/gPwzNXK.jpg',
+      isbn: '9780747581086'
+    },
+    {
+      bookId: 17,
+      title: 'Harry Potter e as Relíquias da Morte',
+      category: 'Fantasia',
+      synopsis:
+        'Harry Potter está de volta para o sétimo ano em Hogwarts, onde ele descobre que Lord Voldemort está atrás das Relíquias da Morte. Harry e seus amigos Rony e Hermione decidem investigar o mistério, mas o garoto acaba descobrindo que as relíquias estão ligadas a sua própria vida.',
+      numberOfPages: 512,
+      author: 'J.K. Rowling',
+      launch: new Date('2007-07-21'),
+      publisher: 'Bloomsbury',
+      language: 'Inglês',
+      edition: 1,
+      cover: 'https://i.imgur.com/299jGIj.jpg',
+      isbn: '9780747591054'
+    }
+  ]
