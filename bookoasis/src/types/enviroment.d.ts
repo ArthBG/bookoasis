1e14f5b59d506fccf57bde3b82f93b2b6957f2e9
ArthBG <arthurborges.senai564@gmail.com>
2024-11-17 15:23:13 -0300

    feat: Add Prisma schema and environment type definitions for database configuration

---
+++ bookoasis/src/types/enviroment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        DATABASE_URL: string
+        NEXTAUTH_SECRET: string
+        NEXT_PUBLIC_URL: string
+        VERIFICATION_URL: string
+        RESEND_API_KEY: string
+        RESEND_EMAIL_FROM: string
+        VERIFICATION_SUBJECT: string
+      }
+    }
+  }
+  export {}
