687387025434945a8da6ffdb1f0a82574e46c35d
ArthBG <arthurborges.senai564@gmail.com>
2024-11-27 23:38:10 -0300

    feat: Update card styling in BooksList component; adjust margins, paddings, and dimensions for improved layout

--- src/components/booksList/booksList.tsx
+++ src/components/booksList/booksList.tsx
@@ -23,14 +23,14 @@ const BooksList = () => {
                 <Card.Root className={styles.divCard} minWidth="20%" maxWidth="20%" overflow="hidden" key={book.id}>
                     <Image src={book.coverURL} className={styles.cover} alt={book.title}  />
                     <Card.Body gap="2">
-                    <Card.Title maxWidth="80%">{book.title}</Card.Title>
-        <Card.Description fontWeight="bold">
+                    <Card.Title fontSize="16px" maxWidth="80%">{book.title}</Card.Title>
+        <Card.Description fontSize="14px" fontWeight="bold">
           {book.author}
         </Card.Description>
-        <Text textStyle="sm" color="gray.500">
+        <Text textStyle="sm" fontSize="13px"  color="gray.500">
           {book.publisher}
         </Text>
-        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+        <Text textStyle="2xl" fontSize="16px" fontWeight="medium" letterSpacing="tight" mt="2">
           $450
         </Text>
       </Card.Body>
