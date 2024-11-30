f03b34791202236e371ddbb0a17edd9990b18780
ArthBG <arthurborges.senai564@gmail.com>
2024-11-30 20:05:06 -0300

    feat: Add Header component to the main page; include title for improved navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import BooksList from "../components/booksList/booksList";
+import Header from "../components/header/header";
 import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <p className={styles.welcometext}>Welcome to the Bookstore</p>
       <BooksList />
     </main>
