191f338547bda50e7d7f840a39fd8260e1b165e9
ArthBG <arthurborges.senai564@gmail.com>
2024-12-01 00:49:52 -0300

    feat: Update Home page layout with new text alignment, button addition, and improved welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/header/header";
 import styles from "./page.module.css";
 import { Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -8,16 +9,20 @@ export default function Home() {
       <Header />
       <div className={styles.container}>
         <div className={styles.texts}>
-        <Text fontSize="2xl" fontWeight="bold" color="white.700" mb={4}>
-          Bem-vindo ao Book Oasis - Seu Refúgio Literário
+        <Text fontSize="50px" fontWeight="bold" color="white.700" width={'80%'} mb={7}>
+          Book Oasis Seu Refúgio Literário
         </Text>
-        <Text fontSize="small" color="white.500" width={'50%'}>
-          Assim como um oásis oferece um escape refrescante no meio do deserto, o Book Oasis oferece um refúgio para os amantes de livros. Mergulhe em nossa vasta coleção de eBooks e encontre sua próxima grande aventura. Seja você um ávido leitor em busca dos últimos best-sellers, clássicos atemporais ou joias escondidas, o Book Oasis é o seu destino final para exploração literária. Deixe-nos ser seu santuário de histórias, onde cada livro é uma nova jornada esperando para ser descoberta.
+        <Text fontSize="small" color="white.500" width={'70%'}>
+        O Book Oasis é um refúgio literário, assim como um oásis no deserto, oferecendo uma vasta coleção de eBooks para explorar, desde best-sellers até clássicos e joias escondidas.
         </Text>
+        <Button variant="solid" width={"fit-content"} borderRadius={20} color="grey" size="xl"  mt={10}>
+          Explore agora
+        </Button>
         </div>
         <div className={styles.image}>
         <img src="../ilustrationHome.png" className={styles.images} alt="Ilustração de uma mulher lendo um livro"/>
       </div>
+
       </div>
     </main>
   );
