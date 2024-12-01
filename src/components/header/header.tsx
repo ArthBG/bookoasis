b1951b3dd8d83b38a5f02cdb7f6e4fa09cf77ed7
ArthBG <arthurborges.senai564@gmail.com>
2024-11-30 23:00:53 -0300

    feat: Update Header component styles; apply gradient background and adjust logo image size

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,8 @@
 import styles from './header.module.css';
-import Image from 'next/image';   
 const Header = () => {
   return (
     <div className={styles.header}>
-      <img src={"../BookOasisWletters.png"}alt="Book Oasis" width={140} height={200} className={styles.imageLogo} />
+      <img src={"../BookOasisWletters.png"}alt="Book Oasis" className={styles.imageLogo} />
       <ul className={styles.nav}>
         <li className={styles.navItem}>
           <a href="#home" className={styles.navLink}>Home</a>
@@ -12,7 +11,7 @@ const Header = () => {
           <a href="#about" className={styles.navLink}>About</a>
         </li>
         <li className={styles.navItem}>
-          <a href="#services" className={styles.navLink}>Services</a>
+          <a href="#carrinho" className={styles.navLink}>Carrinho</a>
         </li>
         <li className={styles.navItem}>
           <a href="#contact" className={styles.navLink}>Contact</a>
