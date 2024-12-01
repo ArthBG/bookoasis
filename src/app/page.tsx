ed4589b88c42c0c65ae0aea8242d9edeeb3f6892
ArthBG <arthurborges.senai564@gmail.com>
2024-12-01 14:55:53 -0300

    style: Update page styles and improve text layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,13 @@ export default function Home() {
       <Header />
       <div className={styles.container}>
         <div className={styles.texts}>
-        <Text fontSize="50px" fontWeight="bold" color="white.700" width={'80%'} mb={7}>
-          Book Oasis Seu Refúgio Literário
+        <Text fontSize="50px" fontWeight="560" color="white" width={'60%'}>
+          Book Oasis 
         </Text>
-        <Text fontSize="small" color="white.500" width={'70%'}>
+        <Text fontSize="50px" fontWeight="400" color="white.500" mt={-5} mb={7}>
+          seu recanto literário
+        </Text>
+        <Text fontSize="14px" color="white.500" width={'80%'}>
         O Book Oasis é um refúgio literário, assim como um oásis no deserto, oferecendo uma vasta coleção de eBooks para explorar, desde best-sellers até clássicos e joias escondidas.
         </Text>
         <Button padding={7} variant="solid" width={"fit-content"} borderRadius={40}  fontSize={17} mt={10}>
@@ -23,7 +26,7 @@ export default function Home() {
         </Button>
         </div>
         <div className={styles.image}>
-        <img src="../ilustrationHome.png" className={styles.images} alt="Ilustração de uma mulher lendo um livro" width={590} height={590} />
+        <img src="../ilustracaoHome.png" className={styles.images} alt="Ilustração de uma mulher lendo um livro" width={1200} height={900} />
       </div>
 
       </div>
