e8b4b1e3b816cef78c7b88a64462bc27adc58930
ArthBG <arthurborges.senai564@gmail.com>
2024-12-02 11:01:50 -0300

    style: Update text colors and button variant in Home component for improved visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
         <Text fontSize="50px" fontWeight="560" color="white" width={'60%'}>
           Book Oasis 
         </Text>
-        <Text fontSize="50px" fontWeight="400" color="white.500" mt={-5} mb={7}>
+        <Text fontSize="50px" fontWeight="400" color="white" mt={-5} mb={7}>
           seu recanto literário
         </Text>
-        <Text fontSize="14px" color="white.500" width={'80%'}>
+        <Text fontSize="14px" color="white" width={'80%'}>
         O Book Oasis é um refúgio literário, assim como um oásis no deserto, oferecendo uma vasta coleção de eBooks para explorar, desde best-sellers até clássicos e joias escondidas.
         </Text>
-        <Button padding={7} variant="solid" width={"fit-content"} borderRadius={40}  fontSize={17} mt={10}>
+        <Button padding={7}  variant="outline" width={"fit-content"} borderRadius={40} border={"#02034b 2px solid"} color={"#02034b"} fontSize={17} mt={10}>
           Explore agora
         </Button>
         </div>
