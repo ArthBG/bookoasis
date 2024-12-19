ebfcfd10643f0b71c1bface816e4bc0d51251292
ArthBG <arthurborges.senai564@gmail.com>
2024-12-19 17:50:52 -0300

    fix: improve error handling in user registration and enhance logging

--- src/actions/registerUser.actions.ts
+++ src/actions/registerUser.actions.ts
@@ -3,25 +3,37 @@ import { User } from '../types/User';
 
 export async function registerUser(user: User) {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    console.log('user', user);
+    console.log('user no actions', user);
     try {
-        const response = await fetch(`${apiUrl}/api/v1/users`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        })
-        if (!response.ok) {
-            throw new Error('HTTP Error! Status: ' + response.status); 
-        }
-        const data = await response.json();
-        return data;
+      const response = await fetch(`${apiUrl}/api/v1/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+  
+      console.log('user stringified', JSON.stringify(user));
+      
+      if (!response.ok) {
+        // Lê o erro do backend como JSON ou texto
+        const errorData = await response.json().catch(() => null);
+        const errorMessage = errorData?.error || 'Erro inesperado';
+  
+        // Lança um erro com status e mensagem
+        throw { status: response.status, message: errorMessage };
+      }
+  
+      const data = await response.json();
+      console.log('data response.json no actions', data);
+      return { data };
     } catch (error) {
-        console.log('[REGISTER_USER]', error);
-        return error;
+      console.error('[REGISTER_USER]', error);
+      const typedError = error as { status?: number; message?: string };
+      return { error: { status: typedError.status || 500, message: typedError.message || 'Erro de conexão' } };
     }
-}
+  }
+  
 
 export async function getAllUsers() {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -32,6 +44,10 @@ export async function getAllUsers() {
                 'Content-Type': 'application/json',
             },
         })
+        if(!response.ok) {
+            const errorData = await response.json(); 
+            return { error: { status: response.status, message: errorData.error } };
+        }
         const data = await response.json();
         return data;
     } catch (error) {
