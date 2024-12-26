c581b881cd2670349bb09ae17ee56c3d86eed65a
ArthBG <arthurborges.senai564@gmail.com>
2024-12-26 20:45:53 -0300

    feat: update token handling to use POST method and improve error responses in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,30 +2,31 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get('refresh_token')?.value;
-
-    if (!token) {
-        const url = new URL('/login', request.url);
-        url.searchParams.set('unauthorized', 'true');
-        return NextResponse.redirect(url);
-    }
+    console.log('middleware:', token);
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/tokenHandler`, {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
+        body: JSON.stringify({ token }),
         credentials: 'include',
     });
+
+    const responseData = await res.json();
+    console.log('middleware res from tokenHandler:', responseData);
 
     if (res.status === 401) {
         const url = new URL('/login', request.url);
         url.searchParams.set('tokenExpired', 'true');
         return NextResponse.redirect(url);
+    } else if (res.status === 404) {
+        return NextResponse.redirect('/login');
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/profile/:path*'], 
+    matcher: ['/profile/:path*'],
 };
